84735ee1848de5318d49db1a042d9487e23eaf6f
Marce Ramirez <marceram87@gmail.com>
2019-12-08 18:07:33 +0100

    Add Toolbar and SideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.scss';
 
-const Toolbar = props => {
-  return (
-    <header className={classes.Toolbar}>
-      <div>MENU</div>
-      <div>LOGO</div>
-      <nav>...</nav>
-    </header>
-  );
-};
+const Toolbar = props => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
 export default Toolbar;
