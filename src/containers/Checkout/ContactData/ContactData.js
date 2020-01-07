e9d7fe5d680512b4ade823ff958d56d6285210e4
Marce Ramirez <marceram87@gmail.com>
2020-01-07 08:07:55 +0100

    Add Form for user data

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import axios from '../../../axios-orders';
+
+import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import classes from './ContactData.module.scss';
+import Input from '../../../components/UI/Input/Input';
+
+class ContactData extends Component {
+  state = {
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'email',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' }
+          ]
+        },
+        value: ''
+      }
+    },
+    loading: false
+  };
+
+  orderHandler = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price
+    };
+    axios
+      .post('/orders.json', order)
+      .then(response => {
+        console.log('Response:', response);
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        console.log('Error:', error);
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    const { orderForm } = this.state;
+    const formElementsArray = [];
+
+    for (let key in orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: orderForm[key]
+      });
+    }
+
+    let form = (
+      <form>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Information</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
